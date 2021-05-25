2340a8780986fa4e79d5b89409b24f6e7dda32af
NirajSalunkhe <47925956+NirajSalunkhe@users.noreply.github.com>
2021-05-25 17:50:51 -0500

    Update config.php

--- Apps/Hospital Management System Project/hospital/hms/doctor/include/config.php
+++ Apps/Hospital Management System Project/hospital/hms/doctor/include/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
+define('DB_USER','user1');
+define('DB_PASS' ,'user');
 define('DB_NAME', 'hms');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
